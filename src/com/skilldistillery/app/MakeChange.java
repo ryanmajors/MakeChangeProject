6701849ef480e2b5ed865aa275083b95bce0eb43
Ryan Majors <growbox.ryan@yahoo.com>
2021-10-08 16:19:59 -0600

    added scanner

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -1,10 +1,13 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class MakeChange {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
 	}
 
 }
