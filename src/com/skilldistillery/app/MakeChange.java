e4fd89ebe9104563aa5b598fddf7c8a605e4b21e
Ryan Majors <growbox.ryan@yahoo.com>
2021-10-09 12:33:39 -0600

    Added a while loop with quit feature

--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -17,7 +17,6 @@ public class MakeChange {
 
 		Scanner sc = new Scanner(System.in);
 
-
 //		amount inits
 
 		double price = 0;
@@ -36,98 +35,111 @@ public class MakeChange {
 		double total20Dollars;
 		double total50Dollars;
 
+		boolean keepGoing = true;
+
 //		store greeting
 
 		System.out.println("Welcome to my store!");
 
 //		User Story #1
 //		The user is prompted asking for the price of the item.
-
-		System.out.println("");
-		System.out.print("Please enter the price of the item: ");
-		price = sc.nextDouble();
+		while (keepGoing) {
+			System.out.println("");
+			System.out.print("Please enter the price of the item: ");
+			price = sc.nextDouble();
 
 //		User Story #2
 //		The user is then prompted asking how much money was tendered by the customer.
 
-		System.out.print("Please enter amount tendered: ");
-		amtPaid = sc.nextDouble();
+			System.out.print("Please enter amount tendered: ");
+			amtPaid = sc.nextDouble();
 
-		System.out.println("Price of item you entered is: " + price + " Amount tendered: " + amtPaid);
+			System.out.println("Price of item you entered is: " + price + " Amount tendered: " + amtPaid);
 
 //		User Story #3
 //		Display an appropriate message if the customer provided too little money or the exact amount.
 
-		if (amtPaid < price) {
-			System.out.println("You need to provide more money");
-		} else if (amtPaid == price) {
-			System.out.println("You paid the exact amount, no change returned.");
-		} 
-
+			if (amtPaid < price) {
+				System.out.println("You need to provide more money");
+			} else if (amtPaid == price) {
+				System.out.println("You paid the exact amount, no change returned.");
+			}
 
 //		User Story #4
 //		If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer.
-		if (amtPaid > price) {
+			if (amtPaid > price) {
 
-			change = amtPaid - price;
+				change = amtPaid - price;
+				System.out.println("Your change is: " + change);
+
+				total50Dollars = (int) (change / 50);
+				if (total50Dollars > 0) {
+					change = change % 50;
+					System.out.println("This many fifties" + total50Dollars);
+				}
+
+				total20Dollars = (int) (change / 20);
+				if (total20Dollars > 0) {
+					change = change % 20;
+					System.out.println("This many twenties: " + total20Dollars);
+				}
+
+				total10Dollars = (int) (change / 10);
+				if (total10Dollars > 0) {
+					change = change % 10;
+					System.out.println("This many tens: " + total10Dollars);
+				}
+
+				total5Dollars = (int) (change / 5);
+				if (total5Dollars > 0) {
+					change = change % 5;
+					System.out.println("This many fives:  " + total5Dollars);
+				}
+
+				total1Dollars = (int) (change / 1);
+				if (total1Dollars > 0) {
+					System.out.println("This many ones:  " + total1Dollars);
+				}
+				change = change % 1;
+				change *= 100 + .05;
+
+				totalQuarters = (int) (change / 25);
+				if (totalQuarters > 0) {
+					change = change % 25;
+					System.out.println("This many quarters:  " + totalQuarters);
+				}
+
+				totalDimes = (int) (change / 10);
+				if (totalDimes > 0) {
+					change = change % 10;
+					System.out.println("This many dimes: " + totalDimes);
+				}
+
+				totalNickles = (int) (change / 05);
+				if (totalNickles > 0) {
+					change = change % 05;
+					System.out.println("This many nickels: " + totalNickles);
+				}
+
+				totalPennies = (int) (change / 01);
+				if (totalPennies > 0) {
+					change = change % 01;
+					System.out.println("This many pennies:  " + totalPennies);
+				}
+			}
 			
+			System.out.print("Would you like to enter another item? (y/n): ");
+
+			String option = sc.next();
 			
-			total50Dollars = (int) (change / 50);
-			if (total50Dollars > 0) {
-				change = change % 50;
-				System.out.println("This many fifties" + total50Dollars);
+			if(!option.equals("y")) {
+				System.out.println("Thank you for using my store. Have a great day!");
+				keepGoing = false;
 			}
-
-			total20Dollars = (int) (change / 20);
-			if (total20Dollars > 0) {
-				change = change % 20;
-				System.out.println("This many twenties: " + total20Dollars);
-			}
-
-			total10Dollars = (int) (change / 10);
-			if (total10Dollars > 0) {
-				change = change % 10;
-				System.out.println("This many tens: " + total10Dollars);
-			}
-
-			total5Dollars = (int) (change / 5);
-			if (total5Dollars > 0) {
-				change = change % 5;
-				System.out.println("This many fives:  " + total5Dollars);
-			}
-
-			total1Dollars = (int) (change / 1);
-			if (total1Dollars > 0) {
-				System.out.println("This many ones:  " + total1Dollars);
-			}
-			change = change % 1;
-			change *= 100 + .05;
 			
-
-			totalQuarters = (int) (change / 25);
-			if (totalQuarters > 0) {
-				change = change % 25;
-				System.out.println("This many quarters:  " + totalQuarters);
-			}
-
-			totalDimes = (int) (change / 10);
-			if (totalDimes > 0) {
-				change = change % 10;
-				System.out.println("This many dimes: " + totalDimes);
-			}
-
-			totalNickles = (int) (change / 05);
-			if (totalNickles > 0) {
-				change = change % 05;
-				System.out.println("This many nickels: " + totalNickles);
-			}
-
-			totalPennies = (int) (change / 01);
-			if (totalPennies > 0) {
-				change = change % 01;
-				System.out.println("This many pennies:  " + totalPennies);
-			}
 		}
+
+		sc.close();
 	}
 
 }
