91eecfdc0a60286e985ae1289665c60ead5135c5
Ryan Majors <growbox.ryan@yahoo.com>
2021-10-11 07:53:20 -0600

    Final updates for project

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Make Change Project
 
 ## Description
-This is my very first Java project while at SD. This is where I am going to put a really handy Description of what this project does, and how to run it.
+This is my very first Java project while at Skill Distillery. I have created a very basic cash register. After opening the file, you will be prompted whether or not you are ready to checkout. If you are ready, then you will type 1 and hit the enter key. Otherwise, if you hit 2, the program will output a farewell greeting letting you know to restart the program when you are ready to checkout. Once you are ready to checkout, the program will ask you for the amount of the price of the item you wish to purchase followed by the amount that you will pay the cashier. Once you have entered the amount that you will be paying the cashier, the program will output the change due to you after purchase. The change will be displayed in normal dollar and cent format ($0.0) along with the amount of each denomination you will receive back from the cashier. You will then be prompted to decide whether you would like to purchase another item. If you select 'y' you will go through the process again. If you select 'n' the program will quit and output a thank you and goodbye message. Additionally, I have programed into the code, the functionality to determine if you have paid too little or exactly the right amount. If you have paid too little, the program will tell you that you do not have enough and to come back when you do and exit the program. If you pay with exact change, the program will notify you of this and that you will not be getting any change back.
 
 ## What I learned
-This project helped me to understand ...
+This project helped me to understand more deeply, if/else statements. Especially nested statements. I also learned that %.2f is very helpful for converting a long decimal into a 2 decimal place monetary format. Overall, I feel that with this project I have become even more confident in the beginning lessons taught to us at Skill Distillery.
 
 ### Technologies Used
 Java, Eclipse, Git
--- src/com/skilldistillery/app/MakeChange.java
+++ src/com/skilldistillery/app/MakeChange.java
@@ -5,141 +5,230 @@ import java.util.Scanner;
 //		TEST CONDITIONS
 //		Amount: .67, Tendered: .50, Result: Error message
 //		Amount: .67, Tendered: 1.00, Result: 1 quarter, 1 nickel, 3 pennies.
-//		Amount: .59, Tendered: 1.00, Result: 1 quarter, 1 dime, 1 nickel, 1 penny.
+//		Amount: .59, Tendered: 1.00, Result: 1 quarter, 1 dimes, 1 nickel, 1 penny.
 //		Amount: 3.96, Tendered: 20.00, Result: 1 ten dollar bill, 1 five dollar bill, 1 one dollar bill, 4 pennies.
 //		Amount: any amount less than 20.00, Tendered: anything greater than amount: correct denominations for correct change.
 
 public class MakeChange {
 
+//	scanner created for user input
+
+	static Scanner sc = new Scanner(System.in);
+
+//  boolean created for continuous user interaction
+
+	static boolean keepGoing = true;
+
+//  main method to fore code execution	
+
 	public static void main(String[] args) {
 
-//		input scanner to get price and amount tendered
+		// store greeting and checkout option
 
-		Scanner sc = new Scanner(System.in);
+		menu();
+		int checkout = sc.nextInt();
 
-//		amount inits
+		if (checkout == 1) {
+			register();
 
-		double price = 0;
-		double amtPaid = 0;
-		double change = 0;
+		} else if (checkout == 2) {
 
-//		total currency amounts needed for change
+			// quit option message
 
-		double totalPennies;
-		double totalNickles;
-		double totalDimes;
-		double totalQuarters;
-		double total1Dollars;
-		double total5Dollars;
-		double total10Dollars;
-		double total20Dollars;
-		double total50Dollars;
+			System.out.println("Please restart the program when you are ready to checkout!");
+		}
 
-		boolean keepGoing = true;
+	}
+	
+//  Checkout menu 
 
-//		store greeting
+	public static void menu() {
+		System.out.println("***************************************");
+		System.out.println("*                                     *");
+		System.out.println("*                                     *");
+		System.out.println("*    ~ Welcome to Ryan's Grocers ~    *");
+		System.out.println("*                                     *");
+		System.out.println("*      Are you ready to checkout?     *");
+		System.out.println("*                                     *");
+		System.out.println("*  Please type 1 or 2 then hit Enter  *");
+		System.out.println("*                                     *");
+		System.out.println("*     1.   Yes                        *");
+		System.out.println("*                                     *");
+		System.out.println("*     2.   No                         *");
+		System.out.println("*                                     *");
+		System.out.println("*                                     *");
+		System.out.println("***************************************");
+	}
 
-		System.out.println("Welcome to my store!");
+// method created for register functionality
+	
+	public static void register() {
 
-//		User Story #1
-//		The user is prompted asking for the price of the item.
+		// continuous loop until quit
+
 		while (keepGoing) {
+
+			// amount inits
+
+			double price, amtPaid, change;
+
+			// total currency amounts needed for change
+
+			double pennies, nickels, dimes, quarters, oneDollar, fiveDollars, tenDollars, twentyDollars, fiftyDollars, hundredDollars;
+
+			// User Story #1
+			// The user is prompted asking for the price of the item.
+			
 			System.out.println("");
-			System.out.print("Please enter the price of the item: ");
+			System.out.print("Please enter the price of the item: $");
 			price = sc.nextDouble();
 
-//		User Story #2
-//		The user is then prompted asking how much money was tendered by the customer.
+			// User Story #2
+			// The user is then prompted asking how much money was tendered by the customer.
 
-			System.out.print("Please enter amount tendered: ");
+			System.out.print("\nPlease enter amount given to cashier: $");
 			amtPaid = sc.nextDouble();
 
-			System.out.println("Price of item you entered is: " + price + " Amount tendered: " + amtPaid);
+			// (Optional repeat of item price and how much was paid)
+			// System.out.printf("\nThe price of item you entered is: $%.2f\n\nThe amount paid to cashier: $%.2f\n", price, amtPaid);
 
-//		User Story #3
-//		Display an appropriate message if the customer provided too little money or the exact amount.
+			// User Story #3
+			// Display an appropriate message if the customer provided too little money or
+			// the exact amount.
 
 			if (amtPaid < price) {
-				System.out.println("You need to provide more money");
+				System.out.println("\nYou do not have enough money.\n\nPlease restart the program when you have enough.\n\nGoodbye.");
+				break;
+				
+				
 			} else if (amtPaid == price) {
-				System.out.println("You paid the exact amount, no change returned.");
+				System.out.println("\nYou paid the exact amount, no change returned.");
 			}
 
-//		User Story #4
-//		If the amount tendered is more than the cost of the item, display the number of bills and coins that should be given to the customer.
+			// User Story #4
+			// If the amount tendered is more than the cost of the item, display the number
+			// of bills and coins that should be given to the customer.
 			if (amtPaid > price) {
 
 				change = amtPaid - price;
-				System.out.println("Your change is: " + change);
+				System.out.printf("\nYour change is: $%.2f\n", change);
+				System.out.println("\nGiven back to you in the following bills and or coins.\n");
 
-				total50Dollars = (int) (change / 50);
-				if (total50Dollars > 0) {
+				hundredDollars = (int) (change / 50);
+				if (hundredDollars > 0) {
+					change = change % 100;
+					if (hundredDollars > 1) {
+						System.out.println((int) hundredDollars + " Hundred dollar bills");
+						
+					} else {
+						System.out.println((int) hundredDollars + " Hundred dollar bill");
+					}
+				}
+				fiftyDollars = (int) (change / 50);
+				if (fiftyDollars > 0) {
 					change = change % 50;
-					System.out.println("This many fifties" + total50Dollars);
+					if (fiftyDollars > 1) {
+						System.out.println((int) fiftyDollars + " Fifty dollar bills");
+						
+					} else {
+						System.out.println((int) fiftyDollars + " Fifty dollar bill");
+					}
 				}
 
-				total20Dollars = (int) (change / 20);
-				if (total20Dollars > 0) {
+				twentyDollars = (int) (change / 20);
+				if (twentyDollars > 0) {
 					change = change % 20;
-					System.out.println("This many twenties: " + total20Dollars);
+					if(twentyDollars > 1) {
+						System.out.println((int) twentyDollars + " Twenty dollar bills");
+					} else {
+					System.out.println((int) twentyDollars + " Twenty dollar bill");
+					}
 				}
 
-				total10Dollars = (int) (change / 10);
-				if (total10Dollars > 0) {
+				tenDollars = (int) (change / 10);
+				if (tenDollars > 0) {
 					change = change % 10;
-					System.out.println("This many tens: " + total10Dollars);
+					if (tenDollars > 1) {
+						System.out.println((int) tenDollars + " Ten dollar bills");
+					} else {
+						System.out.println((int) tenDollars + " Ten dollar bill");
+						
+					}
 				}
 
-				total5Dollars = (int) (change / 5);
-				if (total5Dollars > 0) {
+				fiveDollars = (int) (change / 5);
+				if (fiveDollars > 0) {
 					change = change % 5;
-					System.out.println("This many fives:  " + total5Dollars);
+					if (fiveDollars > 1) {
+						System.out.println((int) fiveDollars + " Five dollar bills");
+					} else {
+						System.out.println((int) fiveDollars + " Five dollar bill");
+					}
 				}
 
-				total1Dollars = (int) (change / 1);
-				if (total1Dollars > 0) {
-					System.out.println("This many ones:  " + total1Dollars);
+				oneDollar = (int) (change / 1);
+				if (oneDollar > 0) {
+					change = change % 1;
+					if(oneDollar > 1) {
+						System.out.println((int) oneDollar + " One dollar bills");
+					} else {
+						System.out.println((int) oneDollar + " One dollar bill");
+					}
 				}
-				change = change % 1;
 				change *= 100 + .05;
-
-				totalQuarters = (int) (change / 25);
-				if (totalQuarters > 0) {
+				
+				
+				quarters = (int) (change / 25);
+				if (quarters > 0) {
 					change = change % 25;
-					System.out.println("This many quarters:  " + totalQuarters);
+					if (quarters > 1) {
+						System.out.println((int) quarters + " quarters");
+					} else {
+						System.out.println((int) quarters + " quarter");
+					}
 				}
 
-				totalDimes = (int) (change / 10);
-				if (totalDimes > 0) {
+				dimes = (int) (change / 10);
+				if (dimes > 0) {
 					change = change % 10;
-					System.out.println("This many dimes: " + totalDimes);
+					if(dimes > 1) {
+						System.out.println((int) dimes + " dimes");
+					} else {
+						System.out.println((int) dimes + " dime");
+					}
 				}
 
-				totalNickles = (int) (change / 05);
-				if (totalNickles > 0) {
+				nickels = (int) (change / 05);
+				if (nickels > 0) {
 					change = change % 05;
-					System.out.println("This many nickels: " + totalNickles);
+					if(nickels > 1) {
+						System.out.println((int) nickels + " nickels");
+					} else {
+						System.out.println((int) nickels + " nickel");
+					}
 				}
 
-				totalPennies = (int) (change / 01);
-				if (totalPennies > 0) {
+				pennies = (int) (change / 01);
+				if (pennies > 0) {
 					change = change % 01;
-					System.out.println("This many pennies:  " + totalPennies);
+					if(pennies > 1) {
+						System.out.println((int) pennies + " pennies");
+					} else {
+						System.out.println((int) pennies + " penny");
+					}
 				}
 			}
-			
-			System.out.print("Would you like to enter another item? (y/n): ");
+
+			System.out.print("\nWould you like to enter another item? (y/n): ");
 
 			String option = sc.next();
-			
-			if(!option.equals("y")) {
+
+			if (!option.equals("y")) {
 				System.out.println("Thank you for using my store. Have a great day!");
 				keepGoing = false;
 			}
-			
-		}
 
+		}
 		sc.close();
 	}
-
 }
